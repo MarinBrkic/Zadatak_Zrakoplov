15108c7fe319844cbb55627a0ed61b517ad316b5
MarinBrkic <152974525+MarinBrkic@users.noreply.github.com>
2023-12-21 18:16:38 +0100

    Add files via upload

---
+++ Zadatak_Zrakoplov.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zrakoplov
+{
+    internal class Zrakoplov1
+    {
+        static void Main(string[] args)
+        {
+            Zrakoplov zrakoplov = new Zrakoplov();
+
+            zrakoplov.setnaziv("Airbus");
+            zrakoplov.setSnagaMotora(2000);
+            zrakoplov.setDosegLeta(6000);
+
+
+            Console.WriteLine("Naziv zrakoplova: " + zrakoplov.getnaziv() + "\n"
+                + "Snaga motora: " + zrakoplov.getSnagaMotora() + "kw" + "\n"
+                + "Doseg Leta: " + zrakoplov.getDosegLeta() + "km" +  "\n");
+
+            Zrakoplov zrakoplov1 = new Zrakoplov("Cesna SkyCatcher", 75, 870);
+
+            Zrakoplov zrakoplov2 = new Zrakoplov("Pilatus", 1200, 1333);
+
+            Console.WriteLine(zrakoplov1.ToString());
+
+            Console.WriteLine(zrakoplov2.ToString());
+
+            Console.ReadKey();
+        }
+    }
+
+    class Zrakoplov
+    {
+        String naziv;
+        int dosegLeta;
+        double snagaMotora;
+
+        public override string ToString()
+        {
+            string ispis = "Naziv zrakoplova: " + this.getnaziv()
+                + "\n" + "Snaga motora: " + this.getSnagaMotora() + "kw" + "\n"
+                + "DosegLeta: " + this.getDosegLeta() + "km" +  "\n";
+            return ispis;
+        }
+        public Zrakoplov()
+        {
+
+        }
+
+        public Zrakoplov(String naziv, int dosegLeta, double snagaMotora)
+        {
+            this.naziv = naziv;
+            this.dosegLeta = dosegLeta;
+            this.snagaMotora = snagaMotora;
+        }
+
+
+
+        public void setnaziv(String naziv)
+        {
+            this.naziv = naziv;
+        }
+        public String getnaziv()
+        {
+            return this.naziv;
+        }
+        public void setDosegLeta(int dosegLeta)
+        {
+            this.dosegLeta = dosegLeta;
+        }
+        public int getDosegLeta()
+        {
+            return this.dosegLeta;
+        }
+        public void setSnagaMotora(double snagaMotora)
+        {
+            this.snagaMotora = snagaMotora;
+        }
+        public double getSnagaMotora()
+        {
+            return this.snagaMotora;
+        }
+    }
+}
